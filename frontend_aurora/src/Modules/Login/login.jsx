1ef69b26c9254bbdf881ad0a55d316764a1cf48e
kristian kamilo kastillo <cristian.c.castillo1707@gmail.com>
2024-09-23 22:45:28 -0500

    Desenmascarar contra

--- frontend_aurora/src/Modules/Login/login.jsx
+++ frontend_aurora/src/Modules/Login/login.jsx
@@ -1,9 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import FlechaIzquierda from "../../../public/FlechaIzquierda.png"
 import "./login.css"
 
 const LoginPage = () => {
+  const [contra, setContra] = useState(false);
 
+  const enmascararContra = () => {
+    setContra(!contra);
+  };
 
   return (
     <>
@@ -11,35 +15,41 @@ const LoginPage = () => {
         <section className='containerLoginSonOne'>
           <form className='FormLoginSonOne'>
             <section>
-              <h1>login</h1>
+              <h1>Login</h1>
               <p>Ingrese los detalles de su cuenta</p>
             </section>
             <section className='FormLoginSonOneblock'>
 
               <div className="FormLoginSonOneblockOne">
                 <label>
-                  <input 
-                    className="input" type="text" placeholder="Usuario"  required></input>
-                    <hr />
+                  <input className="input" type="text" placeholder="Usuario" maxLength={20} required />
+                  <hr />
                   <i className="bx bxs-user" />
                 </label>
               </div>
-  
+
               <div className="FormLoginSonOneblockTwo">
                 <label>
-                  <input 
-                    className="input" type="password" placeholder="Contraseña" required></input>
-                    <hr />
-                  <i className="bx bxs-lock-alt" />
+                  <input className="input" type={contra ? "text" : "password"} placeholder="Contraseña"
+                    maxLength={25} required />
+                  <hr />
+                  <i className={`bx ${contra ? 'bxs-show' : 'bxs-hide'}`} onClick={enmascararContra}
+                    style={{
+                      cursor: 'pointer', color: '#fff', position: 'absolute', right: '10px',
+                      top: '50%', transform: 'translateY(-50%)'
+                    }} />
                 </label>
               </div>
 
             </section>
-            <button className='FormLoginSonOneButton' type="submit">
-              <img src={FlechaIzquierda} alt="LogoIngresar" width="20px" />
-              <div className='vacio'></div>
-              <p>Entrar</p>
-            </button>
+            <section className='FormLoginSonOneButton'>
+              <button type="submit">
+                <img src={FlechaIzquierda} alt="LogoIngresar" width="20px" />
+                <div className='vacio'></div>
+                <p>Entrar</p>
+              </button>
+            </section>
+
           </form>
         </section>
         <section className='containerLoginSonTwo'></section>
